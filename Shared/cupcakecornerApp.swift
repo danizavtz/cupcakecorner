55dc12ae3e9cf2a1058ca88db7200e413b5d7d5f
danizavtz <daniellucena@yahoo.com.br>
2023-05-05 14:40:23 -0300

    Initial Commit

---
+++ Shared/cupcakecornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  cupcakecornerApp.swift
+//  Shared
+//
+//  Created by Daniel Lucena Pires on 05/05/23.
+//
+
+import SwiftUI
+
+@main
+struct cupcakecornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
